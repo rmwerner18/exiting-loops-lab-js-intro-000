0bf5b2b0a4c8aded0770670e1c27d558099c9d4a
Ryan Werner <rmwerner@umich.edu>
2020-05-19 16:37:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function breakOut(array, changeValue, stopValue) {
     if (array[i] !== stopValue) {
       break
     }
-    array[i] = console.log(array[i] = array[i] - array[i] + changeValue)
+    array[i] = `${changeValue}`
   }
   return array
 }
