8ba22b809584a34cf02aa9f712114530c8e4c16e
Ryan Werner <rmwerner@umich.edu>
2020-05-19 16:16:39 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,8 @@
+function breakout(array, changeValue, stopValue) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] !== stopValue) {
+      break
+    }
+    array[i] = changeValue
+  }
+}
